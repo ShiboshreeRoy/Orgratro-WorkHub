aee2c157425e74fe1c086742aa297f79d02b5e47
shiboshreeroy <shiboshreeroy169@gmail.com>
2025-07-15 07:20:47 +0600

    update project

--- config/routes.rb
+++ config/routes.rb
@@ -26,12 +26,11 @@ Rails.application.routes.draw do
 
 
 
-  
-  devise_for :users, controllers: { 
-    registrations: 'users/registrations' 
-  }
+  devise_for :users, 
+  controllers: { registrations: 'users/registrations' }, 
+  sign_out_via: [:get, :delete]
 
-  
+
   resources :admin , only: [:index, :create, :edit, :update, :show, :destory], controller: 'admin'
   resources :admin_dashbord, only: [:index]
 
