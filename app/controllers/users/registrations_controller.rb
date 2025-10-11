a0579f785603dcf5a6731d4ab001cb3cb5c4db60
ShiboshreeRoy <shiboshreeroy169@gmail.com>
2025-10-11 22:53:21 +0600

    Uppdate project  and add  reffer  system notification

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,31 +2,33 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
 
   def create
+    # Check password confirmation manually
+    if params[:user][:password] != params[:user][:password_confirmation]
+      flash[:alert] = "
+Password and confirmation did not match 😓"
+      redirect_to new_user_registration_path and return
+    end
+
     super do |resource|
-      apply_referral_if_present(resource)
+      apply_referral_if_present(resource) if resource.persisted?
     end
   end
 
+  private
 
-private
+  def apply_referral_if_present(user)
+    token = params[:ref] || params[:referral_token] || session.delete(:referral_token)
+    return unless token.present?
 
+    referral = Referral.find_by(token: token, claimed: false)
+    return unless referral
+    return if referral.referrer_id == user.id
 
-    def apply_referral_if_present(user)
-      token = params[:ref] || params[:referral_token] || session.delete(:referral_token)
-      return unless token.present?
-
-      referral = Referral.find_by(token: token, claimed: false)
-      return unless referral
-      return if referral.referrer_id == user.id
-
-      referral.mark_claimed!(user: user)
-      user.update!(referred_by: referral.referrer)
-    end
-
+    referral.mark_claimed!(user: user)
+    user.update!(referred_by: referral.referrer)
+  end
 
   protected
-
-
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
